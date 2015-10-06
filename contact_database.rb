a3ccd5b00b2d9a3b14a9f17a52dfeb558e1b7f15
Toto <naoki3355@gmail.com>
2015-10-06 21:05:44 +0000

    Use OOP design

--- contact_database.rb
+++ contact_database.rb
@@ -1,2 +1,62 @@
 ## TODO: Implement CSV reading/writing
 require 'csv'
+
+
+class ContactData
+ 
+  attr_reader :contacts
+
+  def initialize(file)
+    @file = file
+    @contacts = []
+
+    read
+  end
+
+  def read
+    @contacts = []
+
+    # Read all contacts from file
+
+    # CSV.foreach('contacts.csv') do |csv|
+    #   # puts "ID: #{csv[0]} Name: #{csv[1]} E-mail: #{csv[2]}" if option
+    #   @contacts << Contact.new() # use field from CSV to create contact
+    # end
+
+    CSV.foreach(@file) do |row|
+      @contacts << Contact.from_row(row)
+    end
+  end
+
+  def write
+    # Write All @contacts to the file
+    # CSV.open(@file, "a") do |csv|  
+    #   csv << @contacts
+    # end
+
+    open csv
+      @contacts.each { |c| csv << c.to_row }
+    end
+  end
+
+  def create(name, email, phone)
+    @contacts << Contact.new(new_id, name, email, phone)
+    write
+  end
+
+  private
+
+  def new_id
+    @contacts.max(:id) + 1
+  end
+
+  # def num_current_contact
+  #   @contacts.length
+  #   # total = 0
+  #   # CSV.foreach('contacts.csv') do |csv|
+  #   #   total += 1
+  #   # end
+  #   # total
+  # end
+
+end
--- contact_list.rb
+++ contact_list.rb
@@ -1,5 +1,44 @@
 require_relative 'contact'
 require_relative 'contact_database'
-
+require 'pry'
 # TODO: Implement command line interaction
 # This should be the only file where you use puts and gets
+
+class ContactInteract
+
+  def initialize
+    @db = ContactData.new('contacts.csv')
+  end
+
+
+  def new_contact
+    puts 'Please enter the full name: '
+    name = gets.chomp
+    puts 'Please enter the contact email: '
+    mail = gets.chomp
+    puts 'Please enter the contact phone type: '
+    label = gets.chomp
+    puts 'Please enter the contact phone number: '
+    phone_number = gets.chomp
+    contact_phone = "#{label}: #{phone_number}"
+    # id_index = ContactData.num_current_contact + 1
+    # binding.pry
+    # contact = [id_index, name, mail, contact_phone]
+    # @db.write(contact)
+
+    @db.create(name, mail, phone)
+
+  end
+
+
+  def show_list
+    puts 'Show all the contacts list: '
+    ContactData.read
+  end
+
+
+end
+
+
+ContactInteract.new.show_list
+
